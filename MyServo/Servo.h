c4722803a377d2b38acbe4a1112815475bc6aa47
LiaoJingyi <liaojingyi2@gmail.com>
2016-04-02 22:22:30 +0800

    fixed indentation and some remark.

--- MyServo/Servo.h
+++ MyServo/Servo.h
@@ -13,46 +13,46 @@
 
 struct Servo
 {
-    int Pos[Servo_Num];//[500,2500]
-    //initialize
-    Servo()
-    { for(int i=0;i<Servo_Num;i++) Pos[i]=Init_PWM; }
-    Servo(const int *In_Data)
-    { for(int i=0;i<Servo_Num;i++) Pos[i]=*(In_Data+i); }
+	int Pos[Servo_Num];//[500,2500]
+	//initialize
+	Servo()
+	{ for(int i=0;i<Servo_Num;i++) Pos[i]=Init_PWM; }
+	Servo(const int *In_Data)
+	{ for(int i=0;i<Servo_Num;i++) Pos[i]=*(In_Data+i); }
 
-    //action
+	//action
 
 	//rotate 18 servos to Pos[] in t ms
 	//Attention: the number labled in servo controller is exact added 1.
-    void Rotate(int t)
-    {
-        Serial Com(p9,p10);
-        for(int i=0;i<Servo_Num;i++) Com.printf("#%dP%d",i+1,Pos[i]);
-        Com.printf("T%d\r\n",t);
-        wait_ms(t);
-    }
-    void Rotate()
-    {
-        Serial Com(p9,p10);
-        for(int i=0;i<Servo_Num;i++) Com.printf("#%dP%d",i+1,Pos[i]);
-        Com.printf("T1000\r\n");
-        wait_ms(1000);
-    }
+	void Rotate(int t)
+	{
+		Serial Com(p9,p10);
+		for(int i=0;i<Servo_Num;i++) Com.printf("#%dP%d",i+1,Pos[i]);
+		Com.printf("T%d\r\n",t);
+		wait_ms(t);
+	}
+	void Rotate()
+	{
+		Serial Com(p9,p10);
+		for(int i=0;i<Servo_Num;i++) Com.printf("#%dP%d",i+1,Pos[i]);
+		Com.printf("T1000\r\n");
+		wait_ms(1000);
+	}
 
 	//basic action for 6 feet robot
-    void Stand()
-    {
+	void Stand()
+	{
 		//Defination of C_PWM and it won't change in the whole program.
 		int C_PWM=1300;
 
-        for(int i=0;i<Servo_Num/3;i++)
-        {
-            Pos[i*3+2]=Init_PWM;//A 1500
-            Pos[i*3+1]=(Init_PWM+Up_PWM)>>1;//B 2000
-            Pos[i*3]=C_PWM;//C 1300
-        }
-        Rotate();
-    }
+		for(int i=0;i<Servo_Num/3;i++)
+		{
+			Pos[i*3+2]=Init_PWM;//A 1500
+			Pos[i*3+1]=(Init_PWM+Up_PWM)>>1;//B 2000
+			Pos[i*3]=C_PWM;//C 1300
+		}
+		Rotate();
+	}
 	//walk action
 	void Walk_towards(int N) //complete N period of walking towards, spending (N*4+2)*Standard_Rotate_Period.
 	{
@@ -145,5 +145,5 @@ struct Servo
 			Rotate();
 		}
 		Stand();
-
-	};
+	}
+};
